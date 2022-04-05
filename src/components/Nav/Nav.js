769a8eafe52d227b180c3db6feb6a786605b8744
ALI <68592145+Ali-awesome@users.noreply.github.com>
2022-04-05 15:57:30 +0600

    Components Are Done

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Nav.css';
+
+
+const Nav = () => {
+    return (
+        <nav className='nav'>
+            <div>
+                <Link to="/home">Home</Link>
+                <Link to="/reviews">Reviews</Link>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/blogs">Blogs</Link>
+                <Link to="/about">About</Link>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>The site You are looking for is not available.</h1>
+        </div>
+    );
+};
+
+export default NotFound;
